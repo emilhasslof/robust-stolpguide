afc94a38577f2f76d867f4d9662963f0ad9317ac
emilhasslof <emil.hasslof@gmail.com>
2023-11-14 21:38:29 +0100

    started working on sprint1

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, Dimensions, StyleSheet, Text, View } from "react-native";
+import * as Svg from "react-native-svg";
+import Logo from "./assets/Logo.svg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Logo width={"100%"} />
       <StatusBar style="auto" />
+      <ToggleMode />
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+function ToggleMode() {
+  return (
+    <View style={styles.ToggleMode}>
+      <Button
+        style={styles.toggleButton}
+        title="Sök"
+        onPress={() => {
+          //Go to search mode
+        }}
+      />
+      <Button
+        style={styles.toggleButton}
+        title="Översätt"
+        onPress={() => {
+          //Go to translate mode
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    top: 32,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "top",
+  },
+  ToggleMode: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: Dimensions.get("window").width,
+    height: "8%",
+    backgroundColor: "grey",
+  },
+  toggleButton: {
+    width: "50%",
+    height: "100%",
+    backgroundColor: "red",
+    cornerRadius: 5,
   },
 });
