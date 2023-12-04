f8f9ad72fee23b24c9ffbc56e147e5ff179ca2e2
emilhasslof <emil.hasslof@gmail.com>
2023-12-04 08:54:37 +0100

    working on dropdown component

---
+++ Dropdown.js
@@ -0,0 +1,41 @@
+import { React, useEffect, useState } from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+
+function Dropdown({ options, textInputRef }) {
+    const [dropdownStyle, setDropdownStyle] = useState({})
+
+    useEffect(() => {
+        if (!textInputRef.current) return
+        textInputRef.current.measure((x, y, width, height, pageX, pageY) => {
+            setDropdownStyle({
+                position: "absolute",
+                top: pageY + height,
+                left: pageX,
+                width: width,
+                height: 200,
+                maxHeight: Dimensions.get("window").height / 2,
+                backgroundColor: "red",
+                zIndex: 1,
+            })
+        })
+    })
+
+    return (
+        <View style={dropdownStyle}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    dropdown: {
+        position: "absolute",
+        width: "40%",
+        height: 200,
+        maxHeight: Dimensions.get("window").height / 2,
+        backgroundColor: "red",
+        zIndex: 1,
+    }
+})
+
+export default Dropdown;
