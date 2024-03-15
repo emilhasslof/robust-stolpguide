8873bec99f6dbd85ca3cc65d7939087438c75a53
emilhasslof <emil.hasslof@gmail.com>
2024-03-15 16:38:40 +0100

    nested scroll works but only inside header area

--- Dropdown.js
+++ Dropdown.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions, FlatList, Pressable, Text } from 'react-native';
+import { View, StyleSheet, Dimensions, FlatList, Pressable, Text, ScrollView } from 'react-native';
 
 function Dropdown({ options, inputPosition, inputString, choiceCallback }) {
     const [data, setData] = useState(options)
@@ -16,7 +16,7 @@ function Dropdown({ options, inputPosition, inputString, choiceCallback }) {
     const styles = StyleSheet.create({
         container: {
             position: "absolute",
-            height: containerHeight,
+            maxHeight: containerHeight,
             overflow: "hidden",
             width: inputPosition.width - 20,
             left: inputPosition.x + 10,
@@ -46,21 +46,21 @@ function Dropdown({ options, inputPosition, inputString, choiceCallback }) {
 
     return (
         <View style={styles.container}>
-            <FlatList
-                nestedScrollEnabled={true}
-                style={styles.flatlist}
-                data={data}
-                horizontal={false}
-                renderItem={({ item }) =>
-                    <Pressable
-                        onPress={() => handlePress(item)}>
-                        <View style={styles.listItem}>
-                            <Text style={styles.text}>{item}</Text>
-                        </View>
-                    </Pressable>}
-                keyExtractor={item => item}
-            />
-
+            <ScrollView nestedScrollEnabled={true}>
+                <FlatList
+                    style={styles.flatlist}
+                    data={data}
+                    horizontal={false}
+                    renderItem={({ item }) =>
+                        <Pressable
+                            onPress={() => handlePress(item)}>
+                            <View style={styles.listItem}>
+                                <Text style={styles.text}>{item}</Text>
+                            </View>
+                        </Pressable>}
+                    keyExtractor={item => item}
+                />
+            </ScrollView>
         </View >
     );
 }
